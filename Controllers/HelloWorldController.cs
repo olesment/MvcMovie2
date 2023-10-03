5334649485522213dd92d539cf3d459b2b23899d
Oskar Lesment <oskar-aleksander.lesment@tptlive.ee>
2023-10-03 20:47:44 +0300

    So far all seems to work

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // PART 333333333
+    //eplace the Index method with the following code:
+    // GET: /HelloWorld/
+    //public string Index()
+    //{
+    //    return "This is my default action...";
+    //}
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // 333333
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+
+    // ADD A CONTROLLER 222222222222222222222222222222222222222222
+    // GET: /HelloWorld/Welcome/ 
+    //Change the Welcome method to include two parameters as shown in the following code
+    //public string Welcome()
+    //{
+    //    return "This is the Welcome action method...";
+    //}
+
+    //public string Welcome(string name, int numTimes = 1)
+    //{
+    //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    //}
+    //success
+
+    //Replace the Welcome method with the following code:
+
+    //    public string Welcome(string name, int ID = 1) // v2lja kommenteeritud sest: tut 3:  The complete HelloWorldController:
+    //    {
+    //        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    //    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿
+//tut 4444444 Update the Models/Movie.cs file with the following code:
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+}
